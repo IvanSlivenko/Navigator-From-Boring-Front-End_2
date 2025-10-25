63fff22f321145dcebec729464821a3508f1e9fd
IvanSlivenko <ivan8822@ukr.net>
2025-10-25 14:08:04 +0300

    25-10-2025-14-08

--- google-maps-project/src/App.js
+++ google-maps-project/src/App.js
@@ -1,11 +1,26 @@
-
+import { Map } from './components/Map'
 import './App.css';
+
+import { useJsApiLoader } from '@react-google-maps/api'
 
 const API_KEY = process.env.REACT_APP_API_KEY
 console.log("API_KEY", API_KEY)
+
+const defaultCenter = {
+  lat: -3.745,
+  lng: -38.523,
+}
+
 const App = () => {
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'API_KEY',
+  })
+
   return (
     <div className="App">
+      {isLoaded ? <Map center={defaultCenter} /> : <h2>Loading</h2>}
 
     </div>
   );
